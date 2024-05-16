f536b00cc32f3dca218fea981eab8f0749f61432
Ben Fung <ben.fung@coxautoinc.com>
2024-05-16 23:45:59 +0100

    refactored base api

--- domain/base_api.py
+++ domain/base_api.py
@@ -1,96 +1,113 @@
-
-import base64
-import logging
-import requests
+from typing import Optional
+import base64, logging, requests
+from requests import Request, Session, Response
 
 
 class BaseApi:
-    def __init__(self, base_url, user, password):
+    def __init__(self, 
+                base_url: str,
+                user: Optional[str] =None,
+                password: Optional[str] =None,
+                auth: Optional[str] = 'basic'):
         self.base_url = base_url
         self.user = user
         self.password = password
-        self.auth = 'Basic ' + str(base64.b64encode((self.user + ":" + self.password).encode("utf-8")).decode('utf-8'))
-        self.header = {
+        if auth == 'basic':
+            self.auth = 'Basic ' + str(base64.b64encode((self.user + ":" + self.password).encode("utf-8")).decode('utf-8'))
+        elif auth == 'bearer':
+            self.auth = 'Bearer ' + str(password)
+
+        self.headers = {
             'Authorization': self.auth,
             'Content-Type': 'application/json'
         }
 
-    def _get_query_with_retry(self, param, header=None):
-        retry = 0
+    def _make_request(self, req: Request) -> Optional[Response]:
+        """_summary_
+
+        Args:
+            req (Request): _description_
+
+        Returns:
+            Optional[Response]: _description_
+        """
         response = None
 
-        while retry != 5:
-            try:
-                url = self.base_url + param
-                response = requests.get(url, headers=header, timeout=10)
-                break
-            except Exception as e:
-                logging.debug(f'Exception querying api "{self.base_url}"')
-                logging.debug(str(type(e)))
-                logging.debug(e)
-            retry += 1
-
-        if response is None:
-            logging.warning(f'Can\'t querying api "{self.base_url}" : fail after 5 retries.')
-
+        try:
+            s = Session()
+            prepped = prepped = req.prepare()
+            response = s.send(prepped)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            logging.error(f'Exception querying api "{req.url}"')
+            logging.debug(str(type(err)))
+            logging.debug(err)
+            print(f'Exception querying api "{req.url}"')
+        except requests.exceptions.HTTPError as errh:
+            logging.error(f'Http error querying api "{req.url}"')
+            logging.debug(str(type(err)))
+            logging.debug(err)
+            print(f'Http error querying api "{req.url}"')
+        except requests.exceptions.ConnectionError as errc:
+            logging.error(f'Connection error querying api "{req.url}"')
+            logging.debug(str(type(err)))
+            logging.debug(err)
+            print(f'Connection error querying api "{req.url}"')
+        except requests.exceptions.Timeout as errt:
+            logging.error(f'Timeout error querying api "{req.url}"')
+            logging.debug(str(type(err)))
+            logging.debug(err)
+            print(f'Timeout error querying api "{req.url}"')
+        
         return response
+        
 
-    def _post_query_with_retry(self, param, header=None, body=None):
-        retry = 0
-        response = None
+    def _get(self, param:str, header:dict, **kwargs: any) -> Optional[Response]:
+        """_summary_
 
-        while retry != 5:
-            try:
-                url = self.base_url + param
-                response = requests.post(url, headers=header, json=body, timeout=10)
-                break
-            except Exception as e:
-                logging.debug(f'Exception querying api "{self.base_url}"')
-                logging.debug(str(type(e)))
-                logging.debug(e)
-            retry += 1
+        Args:
+            param (str): _description_
+            header (dict): _description_
 
-        if response is None:
-            logging.warning(f'Can\'t querying api "{self.base_url}" : fail after 5 retries.')
+        Returns:
+            Optional[Response]: _description_
+        """
+        return self._make_request(Request('GET', self.base_url + param, headers=header, **kwargs))
 
-        return response
+    def _post(self, param:str, header:dict,**kwargs: any) -> Optional[Response]:
+        """_summary_
 
-    def _patch_query_with_retry(self, param, header=None, body=None):
-        retry = 0
-        response = None
+        Args:
+            param (str): _description_
+            header (dict): _description_
+            body (dict): _description_
 
-        while retry != 5:
-            try:
-                url = self.base_url + param
-                response = requests.patch(url=url, headers=header, json=body, timeout=10)
-                break
-            except Exception as e:
-                logging.debug(f'Exception querying api "{self.base_url}"')
-                logging.debug(str(type(e)))
-                logging.debug(e)
-            retry += 1
+        Returns:
+            Optional[Response]: _description_
+        """
+        return self._make_request(Request('POST', self.base_url + param, headers=header, **kwargs))
 
-        if response is None:
-            logging.warning(f'Can\'t querying api "{self.base_url}" : fail after 5 retries.')
+    def _patch(self, param:str, header:dict, body:dict, **kwargs: any) -> Optional[Response]:
+        """_summary_
 
-        return response
+        Args:
+            param (str): _description_
+            header (dict): _description_
+            body (dict): _description_
 
-    def _put_query_with_retry(self, param, header=None):
-        retry = 0
-        response = None
+        Returns:
+            Optional[Response]: _description_
+        """
+        return self._request_handler(Request('PATCH', self.base_url + param, headers=header, json=body, **kwargs))
 
-        while retry != 5:
-            try:
-                url = self.base_url + param
-                response = requests.put(url, headers=header, timeout=10)
-                break
-            except Exception as e:
-                logging.debug(f'Exception querying api "{self.base_url}"')
-                logging.debug(str(type(e)))
-                logging.debug(e)
-            retry += 1
+    def _put(self, param:str, header:dict, **kwargs: any) -> Optional[Response]:
+        """_summary_
 
-        if response is None:
-            logging.warning(f'Can\'t querying api "{self.base_url}" : fail after 5 retries.')
+        Args:
+            param (str): _description_
+            header (dict): _description_
 
-        return response
+        Returns:
+            Optional[Response]: _description_
+        """
+        return self._request_handler(Request('PUT', self.base_url + param, headers=header, **kwargs))
